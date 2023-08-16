f1b6aa3196bfd2b721d1a7fef438ceb682e8f763
Pushp Ranjan Karn <prkpushp@gmail.com>
2023-08-17 05:04:38 +0530

    Added User Created

--- dfssetup.ps1
+++ dfssetup.ps1
@@ -1,11 +1,11 @@
 Import-Module DFSR
-$replicationGroupName = "repgrp14"
+$replicationGroupName = "repgrp09"
 $domainName = "win.mykronos.com"
 $serverNames = "WIN-DFS01", "WIN-DFS02", "WIN-DFS03"
 $topologyType = "FullMesh"
 $primaryMember = "WIN-DFS01"
-$folderName = "p14-app"
-$namespace="testnamespc14"
+$folderName = "p09-app"
+$namespace="testnamespc09"
 $node1 = "WIN-DFS01"
 $node2 = "WIN-DFS02"
 $node3 = "WIN-DFS03"
@@ -13,6 +13,13 @@ $dnssuffix = "int.oss.mykronos.com"
 
 foreach ($server in $serverNames) {
     Install-windowsfeature FS-DFS-Namespace, FS-DFS-Replication -IncludeManagementTools -restart
+    $userName = "NewUserew1234"           # Replace with the desired username
+    $password = "gartxi@1234598@jhjd@"       # Replace with the desired password
+    $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+    New-LocalUser -Name $userName -Password $securePassword -Description "SQL Admin User"
+    Start-Sleep -Seconds 5
+    Add-LocalGroupMember -Group "Administrators" -Member $userName
+    Write-Host "User '$userName' created and added to Administrators group."
 }
 
 New-Item -ItemType Directory -Path "C:\DFSRoots\$namespace" 
@@ -25,7 +32,7 @@ New-DfsnFolder -Path "\\$domainName\$namespace\$folderName" -TargetPath "\\$node
 
 
 New-DfsReplicationGroup -GroupName $replicationGroupName -DomainName $domainName
-
+sleep 5
 foreach ($server in $serverNames) {
     Add-DfsrMember -GroupName $replicationGroupName -ComputerName $server
 }
@@ -37,7 +44,10 @@ Add-DfsrConnection -DestinationComputerName $node3 -GroupName $replicationGroupN
 Add-DfsrConnection -DestinationComputerName $node1 -GroupName $replicationGroupName -SourceComputerName $node2
 Add-DfsrConnection -DestinationComputerName $node1 -GroupName $replicationGroupName -SourceComputerName $node3
 Add-DfsrConnection -DestinationComputerName $node2 -GroupName $replicationGroupName -SourceComputerName $node3
+
 New-DfsReplicatedFolder -FolderName "$folderName" -GroupName "$replicationGroupName" -DfsnPath "\\$domainName\$namespace\$folderName"
+
+
 Set-DfsrMembership -ComputerName $node1 -FolderName $folderName -GroupName $replicationGroupName -ContentPath "C:\$folderName" -PrimaryMember $true -Force
 Set-DfsrMembership -ComputerName $node2 -FolderName $folderName -GroupName $replicationGroupName -ContentPath "C:\$folderName" -Force
 Set-DfsrMembership -ComputerName $node3 -FolderName $folderName -GroupName $replicationGroupName -ContentPath "C:\$folderName" -Force
